fe7a722b333010480a8ccc3b9c6196485f37f801
Felix Kleinke <felix_affe@MacBookPro.fritz.box>
2026-09-05 01:37:31 +0200

    Add xcodegen project.yml for auto Xcode project generation

---
+++ ios_app/Bambucontroller IOS/Bambucontroller IOS/Bambucontroller_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bambucontroller_IOSApp.swift
+//  Bambucontroller IOS
+//
+//  Created by Felix Kleinke on 05.09.26.
+//
+
+import SwiftUI
+
+@main
+struct Bambucontroller_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
