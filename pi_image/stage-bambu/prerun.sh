575abce73c6a308c0112b07fd7a5e7b2d8450742
Felix Kleinke <felix_affe@MacBookPro.fritz.box>
2026-09-05 13:57:21 +0200

    Slim repo tarball: exclude build artifacts from image

--- pi_image/stage-bambu/prerun.sh
+++ pi_image/stage-bambu/prerun.sh
@@ -4,5 +4,7 @@
 cd "$(dirname "$0")/../.."
 tar --exclude='.git' --exclude='__pycache__' --exclude='.DS_Store' \
   --exclude='pi_image/stage-bambu/bambu-repo.tar.gz' \
+  --exclude='pi_image/pi-gen' --exclude='ios_app/build' \
+  --exclude='*.xcuserdata' \
   -czf pi_image/stage-bambu/bambu-repo.tar.gz .
 echo "Repo-Tarball: $(du -h pi_image/stage-bambu/bambu-repo.tar.gz | cut -f1)"
